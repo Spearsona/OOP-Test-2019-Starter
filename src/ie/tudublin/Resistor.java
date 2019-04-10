a9aaa5aa26ef6283f4a012552defd35c0a528945
Spearsona <C17753589@mydit.ie>
2019-04-10 17:54:18 +0100

    final edits...

--- src/ie/tudublin/Resistor.java
+++ src/ie/tudublin/Resistor.java
@@ -16,9 +16,9 @@ class Resistor extends PApplet
         int tens = (value - (hundreds * 100)) / 10;
         int ones = value - ((hundreds * 100)  + (tens * 10));
 
-        print(hundreds + ",");
-        print(tens + ",");
-        println(ones);
+        //print(hundreds + ",");
+        //print(tens + ",");
+        //println(ones);
 
     }
 
